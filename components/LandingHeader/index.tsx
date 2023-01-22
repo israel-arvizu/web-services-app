59f25bc4a379113180066c03b020f9f9db434872
Israel Arvizu <israelarvizuofficial@gmail.com>
2023-01-21 17:44:00 -0800

    some small HEAD updates

--- components/LandingHeader/index.tsx
+++ components/LandingHeader/index.tsx
@@ -3,7 +3,7 @@ import classes from "./LandingHeader.module.css"
 export default function LandingText(){
     return (
         <div className={classes.Header_Text}>
-            <div><h1>Turning ideas into real life products is my calling</h1></div>
+            <div><h1>turning ideas into real life products is my calling</h1></div>
             <div><a>View ideas</a></div>
         </div>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
+
+import Head from 'next/head';
 import '../styles/globals.css'
 import "../styles/splashPage.css"
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+            <title>Israel's Web Services</title>
+            <link href="https://fonts.cdnfonts.com/css/mark-pro" rel="stylesheet" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
