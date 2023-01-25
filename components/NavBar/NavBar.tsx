966d8428d53f0246e236d6a4612670974e454857
Israel Arvizu <israelarvizuofficial@gmail.com>
2023-01-24 21:27:59 -0800

    updated splash page styling

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,8 +3,8 @@ import classes from "./NavBar.module.css";
 export default function NavBar(){
     return(
         <div className={classes.nav_bar}>
-            <div><a href="#">Israel Arvizu</a></div>
-            <div><a>Services</a></div>
+            <div><a href="#">ISRAEL ARVIZU</a></div>
+            <div><a>SERVICES</a></div>
         </div>
     )
 }
