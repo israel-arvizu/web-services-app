bee9e0071c2b4fc8eb6cb42fcccf881543ed205c
Israel Arvizu <israelarvizuofficial@gmail.com>
2023-01-31 23:55:20 -0800

    changed scrollbar styling

--- components/LandingHeader/index.tsx
+++ components/LandingHeader/index.tsx
@@ -4,7 +4,7 @@ export default function LandingText(){
     return (
         <div className={classes.Header_Text}>
             <div><h1>turning ideas into <br></br>real life <span>products</span></h1></div>
-            <div><a>VIEW IDEAS</a></div>
+            <div><a href="#section-2-main">VIEW IDEAS</a></div>
         </div>
     )
 }
--- components/ServicesSection/index.tsx
+++ components/ServicesSection/index.tsx
@@ -2,7 +2,7 @@ import classes from "./ServicesSection.module.css"
 
 export default function ServicesSection(){
     return(
-        <section>
+        <section id="section-2-main">
             <div className={classes.services_wrapper}>
                 <h1>Services</h1>
             </div>
