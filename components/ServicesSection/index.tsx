5cc1ebe20bafcb0f2a2ed094708abafbd8a5a9b4
Israel Arvizu <israelarvizuofficial@gmail.com>
2023-02-02 23:10:55 -0800

    updated services styling

--- components/ServicesSection/index.tsx
+++ components/ServicesSection/index.tsx
@@ -7,24 +7,45 @@ export default function ServicesSection(){
                 <h1>Services</h1>
                 <div className={classes.row}>
                     <div className={classes.column}>
-                        <div><h3>1</h3></div>
+                        <div><h3>01</h3></div>
                         <h3>Starter</h3>
                         <p>Perfect to start your digital footprint</p>
+                        <ul>
+                            <li>One Page Site</li>
+                            <li>Hosting & Doming Setup</li>
+                            <li>Custom Design</li>
+                        </ul>
                     </div>
                     <div className={classes.column}>
-                        <div><h3>2</h3></div>
+                        <div><h3>02</h3></div>
                         <h3>Business+</h3>
                         <p>Ready to improve your business?</p>
+                        <ul>
+                            <li>Live feed of </li>
+                            <li>DOnt worry about it</li>
+                            <li>yo yo yo</li>
+                        </ul>
                     </div>
                     <div className={classes.column}>
-                        <div><h3>3</h3></div>
+                        <div><h3>03</h3></div>
                         <h3>Premium</h3>
                         <p>Elevate your online presence</p>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+
                     </div>
                     <div className={classes.column}>
-                        <div><h3>4</h3></div>
+                        <div><h3>04</h3></div>
                         <h3>Deluxe</h3>
                         <p>Unlease the full potential of your online brand</p>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
                     </div>
                 </div>
             </div>
