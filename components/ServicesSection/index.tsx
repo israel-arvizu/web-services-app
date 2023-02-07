6592e2a4179b0f428925d6fc3013d7fbb959cf92
Israel Arvizu <israelarvizuofficial@gmail.com>
2023-02-06 23:58:18 -0800

    updated services styling, still in progress

--- components/ServicesSection/index.tsx
+++ components/ServicesSection/index.tsx
@@ -15,37 +15,40 @@ export default function ServicesSection(){
                             <li>Hosting & Doming Setup</li>
                             <li>Custom Design</li>
                         </ul>
+                        <h2>$149.00</h2>
                     </div>
                     <div className={classes.column}>
                         <div><h3>02</h3></div>
                         <h3>Business+</h3>
                         <p>Ready to improve your business?</p>
                         <ul>
-                            <li>Live feed of </li>
-                            <li>DOnt worry about it</li>
-                            <li>yo yo yo</li>
+                            <li>Up to 5 Pages</li>
+                            <li>Hosting & Domain Setup</li>
+                            <li>Up to 3 Re-Designs</li>
                         </ul>
+                        <h2>$299.00</h2>
                     </div>
                     <div className={classes.column}>
                         <div><h3>03</h3></div>
                         <h3>Premium</h3>
                         <p>Elevate your online presence</p>
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>Up to 10 pages</li>
+                            <li>One Database configuration</li>
+                            <li>6-month Site Support</li>
                         </ul>
-
+                        <h2>Get a Quote</h2>
                     </div>
                     <div className={classes.column}>
                         <div><h3>04</h3></div>
                         <h3>Deluxe</h3>
                         <p>Unlease the full potential of your online brand</p>
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>Unlimited Pages</li>
+                            <li>3 Database Configurations</li>
+                            <li>1-year Site Support</li>
                         </ul>
+                        <h2>Get a Quote</h2>
                     </div>
                 </div>
             </div>
