92d117b3995947ba65b73036291a14cdbbfe5c71
Israel Arvizu <israelarvizuofficial@gmail.com>
2023-02-15 23:57:49 -0800

    added some changes to footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,10 +4,27 @@ export default function Footer(){
     return(
        <div className={classes.footer_container}>
             <div className={classes.footer_left_section}>
+                <p>Designed & Developed by:</p>
                 <h2>Israel Arvizu</h2>
             </div>
+            <div className={classes.footer_middle_section}>
+                <a><i className="fa-brands fa-linkedin-in"></i></a>
+                <a><i className="fa-brands fa-github"></i>bruh</a>
+                <a><i className="fa-regular fa-star"></i>Email</a>
+                <a>Portfolio</a>
+            </div>
             <div className={classes.footer_right_section}>
-
+                <h3>Hours</h3>
+                <table>
+                    <tr>
+                        <th>Weekdays</th>
+                        <th>Weekends</th>
+                    </tr>
+                    <tr>
+                        <td>9am-5pm</td>
+                        <td>10am-3pm</td>
+                    </tr>
+                </table>
             </div>
        </div>
     )
